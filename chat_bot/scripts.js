6ec27ff22620e2b1705f559cbbea48e6b36924ee
dip-mandal <167052758+dip-mandal@users.noreply.github.com>
2025-07-08 11:43:02 +0530

    Update scripts.js

--- chat_bot/scripts.js
+++ chat_bot/scripts.js
@@ -20,7 +20,7 @@ function sendMessage() {
 function getBotResponse(message) {
     const chatBox = document.getElementById("chat-box");
 
-    fetch('https://a261bf6972bf.ngrok-free.app/', {
+    fetch('https://dipmandal303.pythonanywhere.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
